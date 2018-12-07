7df510711407a11f759cdc0b3780340808c22f61
SwechchhaSinha <sinhaswechchha07@gmail.com>
2018-12-07 11:14:39 +0530

    compile

--- src/App.js
+++ src/App.js
@@ -20,58 +20,66 @@ var defaults = {
 
 var App = createReactClass({
 
-  
-	getInitialState: function() {
+
+	getInitialState: function () {
 		return {
 			code: defaults.markdown,
 			readOnly: false,
-      mode: 'markdown',
-      
+			mode: 'markdown',
+
 		};
 	},
-	updateCode: function(newCode) {
+	updateCode: function (newCode) {
 		this.setState({
-      code: newCode,
-      
+			code: newCode,
+
 		});
-  },
-  changeMode (e) {
+	},
+	compile:function()
+	{
+
+
+	},
+	changeMode(e) {
 		var mode = e.target.value;
 		this.setState({
 			mode: mode,
 			code: defaults[mode]
 		});
 	},
-	toggleReadOnly () {
+	toggleReadOnly() {
 		this.setState({
 			readOnly: !this.state.readOnly
 		}, () => this.refs.editor.focus());
-  },
-  matchbrackets(){
-    this.setState({
-      matchbrackets:true
-    })
+	},
+	
+	matchbrackets() {
+		this.setState({
+			matchbrackets: true
+		})
 
-  },
-	render: function() {
+	},
+	render: function () {
 		var options = {
-      lineNumbers: true,
-      matchbrackets:true,
-      autoCloseBrackets :true,
+			lineNumbers: true,
+			matchbrackets: true,
+			autoCloseBrackets: true,
 			readOnly: this.state.readOnly,
 			mode: this.state.mode
 		};
 		return (<div>
-      <CodeMirror ref="editor" value={this.state.code} onChange={this.updateCode} options={options} autoFocus={true} />
-      <div style={{ marginTop: 10 }}>
-					<select onChange={this.changeMode} value={this.state.mode}>
-						<option value="markdown">Markdown</option>
-						<option value="text/x-java">Java</option>
-					</select>
-					<button onClick={this.toggleReadOnly}>Toggle read-only mode (currently {this.state.readOnly ? 'on' : 'off'})</button>
-				</div>
-      </div>
-    )}
+			<CodeMirror ref="editor" value={this.state.code} onChange={this.updateCode} options={options} autoFocus={true} />
+			<div style={{ marginTop: 10 }}>
+				<select onChange={this.changeMode} value={this.state.mode}>
+					<option value="markdown">Markdown</option>
+					<option value="text/x-java">Java</option>
+				</select>
+				<button onClick={this.toggleReadOnly}>Toggle read-only mode (currently {this.state.readOnly ? 'on' : 'off'})</button>
+				<button onClick={this.compile}>Compile</button>
+			</div>
+		</div>
+		)
+	}
 });
 
 
